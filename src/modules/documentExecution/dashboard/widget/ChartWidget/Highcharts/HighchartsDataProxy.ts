2790d9ab6da9e1f8740281a2191fe762ead36e47
dbulatovicx32 <darko.bulatovic@eng.it>
2023-06-01 13:09:07 +0200

    Highcharts Scatter Data Proxy
    Implemented data proxy.

--- src/modules/documentExecution/dashboard/widget/ChartWidget/Highcharts/HighchartsDataProxy.ts
+++ src/modules/documentExecution/dashboard/widget/ChartWidget/Highcharts/HighchartsDataProxy.ts
@@ -4,6 +4,7 @@ import { IDashboardDataset, IWidget, ISelection } from '../../../Dashboard'
 import { getPieChartData } from '../../../DataProxyHelper'
 // import { addDriversToData, addParametersToData, addSelectionsToData, showGetDataError } from '../../../DashboardDataProxy'
 import { getHighchartsBarData } from './dataProxy/HighchartsBarDataProxy'
+import { getHighchartsScatterData } from './dataProxy/HighchartsScatterDataProxy'
 
 export const getHighchartsWidgetData = async (dashboardId, widget: IWidget, datasets: IDashboardDataset[], $http: any, initialCall: boolean, selections: ISelection[], associativeResponseSelections?: any) => {
     const chartType = widget.settings.chartModel?.model?.chart.type
@@ -14,6 +15,8 @@ export const getHighchartsWidgetData = async (dashboardId, widget: IWidget, data
         case 'column':
         case 'line':
             return await getHighchartsBarData(dashboardId, widget, datasets, $http, initialCall, selections, associativeResponseSelections)
+        case 'scatter':
+            return await getHighchartsScatterData(dashboardId, widget, datasets, $http, initialCall, selections, associativeResponseSelections)
         case 'pie':
             return await getPieChartData(widget, datasets, $http, initialCall, selections, associativeResponseSelections)
         default:
