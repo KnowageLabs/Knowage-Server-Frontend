956864eb2e59f8567dace2dcb4b8f45c6ac6f978
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2023-06-01 10:55:39 +0200

    removed log

--- src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditor.vue
+++ src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditor.vue
@@ -2,7 +2,7 @@
     <Teleport to=".dashboard-container">
         <div class="dashboardEditor">
             <Toolbar class="kn-toolbar kn-toolbar--primary">
-                <template #start> {{ widget.type }} Widget Editor {{ widget.invalid }} </template>
+                <template #start> {{ widget.type }} Widget Editor </template>
                 <template #end>
                     <Button icon="pi pi-save" class="p-button-text p-button-rounded p-button-plain" :disabled="widget.invalid" @click="save" />
                     <Button icon="pi pi-times" class="p-button-text p-button-rounded p-button-plain" @click="close" />
