bba0e4e678ea585beb637287146a2b1f1f5aa34f
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2023-06-27 11:43:09 +0200

    Svg Upload

--- src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/ChartWidget/highcharts/configuration/HighchartsSVGSettings.vue
+++ src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/ChartWidget/highcharts/configuration/HighchartsSVGSettings.vue
@@ -15,7 +15,7 @@
         <div class="input-container p-col-12">
             <Button icon="fas fa-upload fa-1x" class="p-button-text p-button-plain p-ml-2" @click="setSVGUpload" />
             <label for="upload" class="kn-material-input-label">{{ $t('dashboard.widgetEditor.highcharts.svg.upload') }}:</label>
-            <KnInputFile :change-function="setSVGForUpload" :visibility="true" :trigger-input="triggerSVGUpload" />
+            <KnInputFile :change-function="setSVGForUpload" :visibility="true" :trigger-input="triggerSVGUpload" accept=".svg" />
         </div>
     </div>
 </template>
@@ -23,6 +23,9 @@
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import { IWidget } from '@/modules/documentExecution/dashboard/Dashboard'
+import { parse } from 'svg-parser'
+import { mapActions } from 'pinia'
+import mainStore from '@/App.store'
 import KnInputFile from '@/components/UI/KnInputFile.vue'
 import Textarea from 'primevue/textarea'
 
@@ -41,6 +44,7 @@ export default defineComponent({
         this.loadSVGSettings()
     },
     methods: {
+        ...mapActions(mainStore, ['setError']),
         loadSVGSettings() {
             this.model = this.widgetModel
             if (!this.model.settings?.configuration?.svgSettings) this.model.settings.configuration.svgSettings = { definition: '' }
@@ -52,8 +56,43 @@ export default defineComponent({
         },
         setSVGForUpload(event: any) {
             const svgToUpload = event.target.files[0]
-            console.log('--------- TEEEEEEST: ', event.target.files)
-            console.log('--------- SVG TO UPLOAD: ', svgToUpload)
+            console.log('---------- svgToUpload', svgToUpload)
+            if (svgToUpload) {
+                const reader = new FileReader()
+                console.log('---------- reader', reader)
+                reader.onload = (e) => {
+                    console.log('---------- e', e)
+                    const svgString = e.target ? e.target.result : ''
+                    const svg = parse(svgString)
+                    console.log('---------- svg', svg)
+                    const pathElement = this.findNode(svg, 'path')
+                    console.log('---------- pathElement', pathElement)
+                    if (pathElement && pathElement.properties && pathElement.properties.d && this.model) {
+                        console.log('---------- pathElement.properties.d', pathElement.properties.d)
+                        let dAttribute = pathElement.properties.d
+                        dAttribute = dAttribute.replaceAll(/(\r\n|\n|\r)/gm, '')
+                        dAttribute = dAttribute.replaceAll(',', '.')
+                        this.model.settings.configuration.svgSettings = { definition: dAttribute }
+                        this.svgSettings = this.model.settings.configuration.svgSettings
+                        console.log('---------- final dAttribute', dAttribute)
+                    } else {
+                        //this.setError({ title: this.$t('common.error.generic'), msg: this.$t('dashboard.widgetEditor.highcharts.svg.error') })
+                    }
+                }
+                reader.readAsText(svgToUpload)
+            }
+        },
+        findNode(node: any, nodeKey: string) {
+            if (node.tagName === nodeKey) {
+                return node
+            } else if (node.children != null) {
+                let result = null as any
+                for (let i = 0; result == null && i < node.children.length; i++) {
+                    result = this.findNode(node.children[i], nodeKey)
+                }
+                return result
+            }
+            return null
         }
     }
 })
