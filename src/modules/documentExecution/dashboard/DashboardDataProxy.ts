1709ebeb21b7ea11a9769a9ccda5e1e7d01ebddd
dbulatovicx32 <darko.bulatovic@eng.it>
2023-07-11 12:00:54 +0200

    R Widget Data Proxy

--- src/modules/documentExecution/dashboard/DashboardDataProxy.ts
+++ src/modules/documentExecution/dashboard/DashboardDataProxy.ts
@@ -23,6 +23,7 @@ import { getHighchartsBarData } from './widget/ChartWidget/Highcharts/dataProxy/
 import { getChartJSWidgetData } from './widget/ChartWidget/ChartJS/ChartJSDataProxy'
 import { getCePivotData } from './widget/cePivotWidget/cePivotWidgetDataProxy'
 import { getPythonData } from './widget/PythonWidget/PythonWidgetDataProxy'
+import { getRData } from './widget/RWidget/RWidgetDataProxy'
 
 const { t } = i18n.global
 const mainStore = store()
@@ -61,6 +62,8 @@ export const getWidgetData = async (dashboardId: any, widget: IWidget, datasets:
             return await getHighchartsWidgetData(dashboardId, widget, datasets, $http, initialCall, selections, associativeResponseSelections)
         case 'python':
             return await getPythonData(dashboardId, widget, datasets, $http, initialCall, selections, associativeResponseSelections)
+        case 'r':
+            return await getRData(dashboardId, widget, datasets, $http, initialCall, selections, associativeResponseSelections)
         default:
             break
     }
