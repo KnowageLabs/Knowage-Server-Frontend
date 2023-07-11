e08c636184d9234ed52d0a1eab9f8295aa9eb032
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2023-07-11 14:57:13 +0200

    R/Python errors

--- src/modules/documentExecution/dashboard/widget/PythonWidget/PythonWidgetContainer.vue
+++ src/modules/documentExecution/dashboard/widget/PythonWidget/PythonWidgetContainer.vue
@@ -1,5 +1,8 @@
 <template>
     <div id="container" class="kn-cursor-pointer" @click="executeInteractions">
+        <Message v-if="!htmlCode && !loading" class="p-d-flex p-jc-center p-m-2" severity="info" :closable="false">
+            {{ $t('dashboard.widgetEditor.python.error') }}
+        </Message>
         <iframe id="iframe" ref="iframeRef" :srcdoc="htmlCode"></iframe>
     </div>
 </template>
@@ -8,15 +11,17 @@
 import { defineComponent, PropType } from 'vue'
 import { IWidget } from '../../Dashboard'
 import { executeCrossNavigationForWidgetsWithoutSpecificCrossNavigationSettings } from '../interactionsHelpers/InteractionHelper'
+import Message from 'primevue/message'
 
 export default defineComponent({
     name: 'python-widget-container',
-    components: {},
+    components: { Message },
     props: { widgetModel: { type: Object as PropType<IWidget>, required: true }, dashboardId: { type: String, required: true }, editorMode: { type: Boolean }, dataToShow: { type: Object as any, required: true } },
     emits: [],
     data() {
         return {
-            htmlCode: null as any
+            htmlCode: null as any,
+            loading: false
         }
     },
     computed: {},
@@ -28,6 +33,7 @@ export default defineComponent({
     methods: {
         loadData() {
             console.log('----------- dataToShow: ', this.dataToShow)
+            this.loading = true
             this.htmlCode = this.dataToShow ? this.dataToShow.result : null
             const iframeRef = this.$refs.iframeRef as any
             if (iframeRef && this.htmlCode) {
@@ -36,10 +42,12 @@ export default defineComponent({
                 // TODO - ASK ON PEER
                 if (iframeBody) iframeBody.addEventListener('click', this.executeInteractions)
             }
+            this.loading = false
         },
         executeInteractions() {
             // TODO - ASK ON PEER
             console.log('---------  executeInteractions()')
+            if (!this.htmlCode) return
             const crossNavigation = this.widgetModel.settings.interactions.crossNavigation
             if (!crossNavigation.enabled) return
             executeCrossNavigationForWidgetsWithoutSpecificCrossNavigationSettings(crossNavigation, this.dashboardId)
@@ -53,5 +61,6 @@ export default defineComponent({
 #iframe {
     height: 100%;
     width: 100%;
+    border: none;
 }
 </style>
--- src/modules/documentExecution/dashboard/widget/RWidget/RWidgetContainer.vue
+++ src/modules/documentExecution/dashboard/widget/RWidget/RWidgetContainer.vue
@@ -1,5 +1,8 @@
 <template>
     <div id="container" class="kn-cursor-pointer" @click="executeInteractions">
+        <Message v-if="!htmlCode && !loading" class="p-d-flex p-jc-center p-m-2" severity="info" :closable="false">
+            {{ $t('dashboard.rWidgetError') }}
+        </Message>
         <iframe id="iframe" ref="iframeRef" :srcdoc="htmlCode"></iframe>
     </div>
 </template>
@@ -8,15 +11,17 @@
 import { defineComponent, PropType } from 'vue'
 import { IWidget } from '../../Dashboard'
 import { executeCrossNavigationForWidgetsWithoutSpecificCrossNavigationSettings } from '../interactionsHelpers/InteractionHelper'
+import Message from 'primevue/message'
 
 export default defineComponent({
     name: 'r-widget-container',
-    components: {},
+    components: { Message },
     props: { widgetModel: { type: Object as PropType<IWidget>, required: true }, dashboardId: { type: String, required: true }, editorMode: { type: Boolean }, dataToShow: { type: Object as any, required: true } },
     emits: [],
     data() {
         return {
-            htmlCode: null as any
+            htmlCode: null as any,
+            loading: false
         }
     },
     computed: {},
@@ -27,6 +32,7 @@ export default defineComponent({
     unmounted() {},
     methods: {
         loadData() {
+            this.loading = true
             this.htmlCode = this.dataToShow ? this.dataToShow.result : null
             const iframeRef = this.$refs.iframeRef as any
             if (iframeRef) {
@@ -35,10 +41,12 @@ export default defineComponent({
                 // TODO - ASK ON PEER
                 if (iframeBody) iframeBody.addEventListener('click', this.executeInteractions)
             }
+            this.loading = false
         },
         executeInteractions() {
             // TODO - ASK ON PEER
             console.log('------ executeInteractions()')
+            if (!this.htmlCode) return
             const crossNavigation = this.widgetModel.settings.interactions.crossNavigation
             if (!crossNavigation.enabled) return
             executeCrossNavigationForWidgetsWithoutSpecificCrossNavigationSettings(crossNavigation, this.dashboardId)
@@ -52,5 +60,6 @@ export default defineComponent({
 #iframe {
     height: 100%;
     width: 100%;
+    border: none;
 }
 </style>
