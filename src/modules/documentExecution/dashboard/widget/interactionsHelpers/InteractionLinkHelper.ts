0ac6c97d64f3b29943a0cb3dde15b64d72f0cd84
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2023-07-14 12:15:48 +0200

    Drivers Link

--- src/modules/documentExecution/dashboard/widget/interactionsHelpers/InteractionLinkHelper.ts
+++ src/modules/documentExecution/dashboard/widget/interactionsHelpers/InteractionLinkHelper.ts
@@ -1,43 +1,57 @@
-import { ITableWidgetLink, IWidgetInteractionParameter, IWidgetLinks } from "../../Dashboard";
+import { IDashboardDriver, ITableWidgetLink, IWidgetInteractionParameter, IWidgetLinks } from "../../Dashboard";
+import dashboardStore from '@/modules/documentExecution/dashboard/Dashboard.store'
 
 interface IClickedValue { value: string, type: string }
 
 export const openNewLinkTableWidget = (clickedValue: IClickedValue, formattedRow: any, linkOptions: IWidgetLinks, dashboardId: string) => {
-    const formattedLinks = getFormattedLinks(linkOptions, formattedRow)
+    const formattedLinks = getFormattedLinks(linkOptions, formattedRow, dashboardId)
     console.log('--------- openNewLinkTableWidget() - formattedLinks: ', formattedLinks)
     formattedLinks.forEach((formattedLink: { url: string, action: string }) => {
-        if (formattedLink.action === 'blank') window.open(formattedLink.url, '_blank');
+        // if (formattedLink.action === 'blank') window.open(formattedLink.url, '_blank');
     })
 }
 
-const getFormattedLinks = (linkOptions: IWidgetLinks, formattedRow: any) => {
+const getFormattedLinks = (linkOptions: IWidgetLinks, formattedRow: any, dashboardId: string) => {
     const formattedLinks = [] as { url: string, action: string }[]
     linkOptions.links?.forEach((link: ITableWidgetLink) => {
-        const formattedLink = getFormattedLink(link, formattedRow)
+        const formattedLink = getFormattedLink(link, formattedRow, dashboardId)
         if (formattedLink) formattedLinks.push(formattedLink)
     })
     return formattedLinks
 }
 
-const getFormattedLink = (link: ITableWidgetLink, formattedRow: any) => {
+const getFormattedLink = (link: ITableWidgetLink, formattedRow: any, dashboardId: string) => {
     let url = link.baseurl
-    let parameters = link.parameters.length > 0 ? getFormattedParametersUrl(link, formattedRow) : ''
+    let parameters = link.parameters.length > 0 ? getFormattedParametersUrl(link, formattedRow, dashboardId) : ''
     if (parameters) parameters = parameters.substring(0, parameters.length - 1)
     url += `?${parameters}`
 
     return { url: url, action: link.action }
 }
 
-const getFormattedParametersUrl = (link: ITableWidgetLink, formattedRow: any) => {
+const getFormattedParametersUrl = (link: ITableWidgetLink, formattedRow: any, dashboardId: string) => {
     let formattedParametersUrl = ''
+    const dashStore = dashboardStore()
+    const drivers = dashStore.getDashboardDrivers(dashboardId)
+    const driversValuesMap = getFormattedDriverValuesMap(drivers)
     link.parameters.forEach((parameter: IWidgetInteractionParameter) => {
         if (parameter.type === 'static') {
             formattedParametersUrl += `${parameter.name}=${parameter.value}&`
         } else if (parameter.type === 'dynamic') {
             formattedParametersUrl += getFormattedDynamicParameterUrl(parameter, formattedRow)
+        } else if (parameter.type === 'driver') {
+            formattedParametersUrl += getFormattedDriverParameterUrl(parameter, driversValuesMap)
         }
     })
     return formattedParametersUrl
+}
+
+const getFormattedDriverValuesMap = (drivers: IDashboardDriver[]) => {
+    if (!drivers) return {}
+    const driversValuesMap = {}
+    // TODO - Ask about multivalue drivers
+    drivers.forEach((driver: IDashboardDriver) => driversValuesMap[driver.urlName] = driver.value)
+    return driversValuesMap
 }
 
 const getFormattedDynamicParameterUrl = (parameter: IWidgetInteractionParameter, formattedRow: any) => {
@@ -46,3 +60,9 @@ const getFormattedDynamicParameterUrl = (parameter: IWidgetInteractionParameter,
     else if (parameter.column) columnValue = formattedRow[parameter.column].value
     return `${parameter.name}=${columnValue ?? ''}&`
 }
+
+const getFormattedDriverParameterUrl = (parameter: IWidgetInteractionParameter, driversValuesMap: any) => {
+    const value = parameter.driver && driversValuesMap[parameter.driver] ? driversValuesMap[parameter.driver] : ''
+    return `${parameter.name}=${value}&`
+}
+
