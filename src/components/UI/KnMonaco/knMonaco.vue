02ffbce8bfd13526ebff0bbdd74171b9c5425282
Davide Vernassa <davide.vernassa@gmail.com>
2023-08-02 17:41:51 +0200

    Changed monaco loader to prevent memory heap errors

--- src/components/UI/KnMonaco/knMonaco.vue
+++ src/components/UI/KnMonaco/knMonaco.vue
@@ -2,14 +2,34 @@
 import { onMounted, onUnmounted, ref, toRefs, watch } from 'vue'
 import { useDebounceFn, useResizeObserver } from '@vueuse/core'
 
-// Import monaco
-// https://github.com/vitejs/vite/discussions/1791
+import { loader } from '@guolao/vue-monaco-editor'
+
 import * as monaco from 'monaco-editor'
-import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
-import JSONWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-import CSSWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-import HTMLWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
-import TSWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+
+self.MonacoEnvironment = {
+    getWorker(_, label) {
+        if (label === 'json') {
+            return new jsonWorker()
+        }
+        if (label === 'css' || label === 'scss' || label === 'less') {
+            return new cssWorker()
+        }
+        if (label === 'html' || label === 'handlebars' || label === 'razor') {
+            return new htmlWorker()
+        }
+        if (label === 'typescript' || label === 'javascript') {
+            return new tsWorker()
+        }
+        return new editorWorker()
+    }
+}
+
+loader.config({ monaco })
 
 const props = defineProps<{
     language: string
@@ -46,20 +66,6 @@ const emit = defineEmits<{
     (e: 'update:modelValue', payload: typeof editorValue.value): void
     (e: 'stringInserted'): void
 }>()
-
-self.MonacoEnvironment = {
-    getWorker(_: string, label: string) {
-        if (label === 'json') return new JSONWorker()
-
-        if (label === 'css' || label === 'scss' || label === 'less') return new CSSWorker()
-
-        if (label === 'html' || label === 'handlebars' || label === 'razor') return new HTMLWorker()
-
-        if (label === 'typescript' || label === 'javascript') return new TSWorker()
-
-        return new EditorWorker()
-    }
-}
 
 const container = ref<HTMLDivElement | null>(null)
 
