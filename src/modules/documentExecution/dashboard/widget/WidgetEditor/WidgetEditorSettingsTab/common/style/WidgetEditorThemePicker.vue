c41a122cbee138f1bdf65d374bba6c76000e3c8f
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2023-10-05 14:41:15 +0200

    Fix

--- src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/common/style/WidgetEditorThemePicker.vue
+++ src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/common/style/WidgetEditorThemePicker.vue
@@ -1,8 +1,9 @@
 <template>
     <div v-if="widget?.settings?.style" class="p-grid p-p-4">
+        {{ widget.settings.style }}
         <div class="p-col-12 p-d-flex p-flex-column p-pb-2">
             <label class="kn-material-input-label">{{ $t('dashboard.widgetEditor.widgetTheme') }}</label>
-            <Dropdown v-model="widget.settings.style.selectedTheme" class="kn-material-input kn-full-width" :options="themes" option-label="themeName" option-value="themeName" @change="onThemeSelected"> </Dropdown>
+            <Dropdown v-model="widget.settings.style.themeName" class="kn-material-input kn-full-width" :options="themes" option-label="themeName" option-value="themeName" @change="onThemeSelected"> </Dropdown>
         </div>
     </div>
 </template>
@@ -31,7 +32,7 @@ export default defineComponent({
     computed: {},
     watch: {
         styleChangedFlag() {
-            if (this.widget) this.widget.settings.style.selectedTheme = null
+            if (this.widget) this.widget.settings.style.themeName = null
         }
     },
     created() {
