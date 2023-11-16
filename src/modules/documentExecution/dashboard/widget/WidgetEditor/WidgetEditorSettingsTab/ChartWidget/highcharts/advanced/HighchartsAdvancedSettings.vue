29d3d9cd5d810f06953ee5b164396192d3bbb99d
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2023-11-16 10:36:01 +0100

    AdvancedChartSettings start

---
+++ src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/ChartWidget/highcharts/advanced/HighchartsAdvancedSettings.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>IT WORKS</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { IWidget } from '@/modules/documentExecution/dashboard/Dashboard'
+
+export default defineComponent({
+    name: 'highcharts-advanced-settings',
+    props: { propWidgetModel: { type: Object as PropType<IWidget>, required: true } },
+    data() {
+        return {
+            widgetModel: null as IWidget | null
+        }
+    },
+    created() {
+        this.loadModel()
+    },
+    methods: {
+        loadModel() {
+            this.widgetModel = this.propWidgetModel
+        }
+    }
+})
+</script>
