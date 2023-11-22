ade26ad91b28eb1b49969e2c38171d9b5bc02bed
BojanSovticEngIT <81489218+BojanSovticEngIT@users.noreply.github.com>
2023-11-22 16:29:23 +0100

    Custom Header Editor changes

--- src/modules/documentExecution/dashboard/generalSettings/DashboardGeneralSettingsHelper.ts
+++ src/modules/documentExecution/dashboard/generalSettings/DashboardGeneralSettingsHelper.ts
@@ -5,7 +5,7 @@ export const createCustomHeaderWidget = () => {
     createNewWidgetSettings(customHeaderWidget)
     customHeaderWidget.new = false
     customHeaderWidget.settings.isCustomDashboardHeader = true
-    customHeaderWidget.settings.configuration.customDashboardHeaderConfiguration = { height: '25px' }
+    customHeaderWidget.settings.configuration.customDashboardHeaderConfiguration = { height: '60px' }
     delete customHeaderWidget.settings.responsive
     return customHeaderWidget
 }  
