f35ae45efff8fc9b5f29ac3e3dc18d406aebb601
Davide Vernassa <davide.vernassa@gmail.com>
2024-01-22 13:55:36 +0100

    [KNOWAGE-8378] Changed global height from vh to %

--- src/App.vue
+++ src/App.vue
@@ -316,7 +316,7 @@ body {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    min-height: 100vh;
+    min-height: 100%;
 }
 .layout-wrapper-content::after {
     content: '';
