fdd054bd8c6b6f6febae0bc0a9ea246d57ba02d9
Davide Vernassa <davide.vernassa@eng.it>
2024-07-01 14:38:03 +0200

    Added conditional existance to meta routes

--- src/App.routes.js
+++ src/App.routes.js
@@ -104,10 +104,10 @@ router.beforeEach(async (to, from, next) => {
             next()
         })*/
     } else {
-        if (to.meta.functionality) {
-            if (from.path === '/' && !store.user?.functionalities?.includes(to.meta.functionality)) await sleep(1000)
+        if (to.meta?.functionality) {
+            if (from.path === '/' && !store.user?.functionalities?.includes(to.meta?.functionality)) await sleep(1000)
         }
-        if (to.meta.functionality && !store.user?.functionalities?.includes(to.meta.functionality)) next({ replace: true, name: '404' })
+        if (to.meta?.functionality && !store.user?.functionalities?.includes(to.meta?.functionality)) next({ replace: true, name: '404' })
         else next()
     }
 })
