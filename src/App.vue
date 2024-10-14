a48f788576afb408a6a34196fe01dd9e38656ec1
Davide Vernassa <davide.vernassa@eng.it>
2024-10-14 17:55:51 +0200

    Restored missing setLoading

--- src/App.vue
+++ src/App.vue
@@ -162,6 +162,7 @@ export default defineComponent({
                     this.stopExecution = true
                 } else auth.logout()
             })
+            .finally(() => this.setLoading(false))
         if (this.stopExecution) return
 
         await this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + '/restful-services/1.0/user-configs').then(async (response: any) => {
