4de94be00b07b9dc91ce4e890c5ad2b3c91b2808
Davide Vernassa <davide.vernassa@eng.it>
2024-12-16 16:21:47 +0100

    [KNOWAGE-8715] Changed download button visibility condition

--- src/helpers/commons/authHelper.ts
+++ src/helpers/commons/authHelper.ts
@@ -6,13 +6,14 @@ export default {
         localStorage.clear()
         store.storeClearIndexedDBCache()
         store.setUser({})
-        window.location.href = `${window.location.origin}/knowage/${isPublicUser ? 'public/' : ''}servlet/AdapterHTTP?ACTION_NAME=LOGOUT_ACTION&LIGHT_NAVIGATOR_DISABLED=TRUE&NEW_SESSION=TRUE`
+        const url = window.location.origin
+        window.location.href = `${url}${import.meta.env.VITE_KNOWAGE_CONTEXT}/${isPublicUser ? 'public/' : ''}servlet/AdapterHTTP?ACTION_NAME=LOGOUT_ACTION&LIGHT_NAVIGATOR_DISABLED=TRUE&NEW_SESSION=TRUE`
     },
     handleUnauthorized(): void {
         const store = mainStore()
         localStorage.clear()
         store.setUser({})
         const url = window.location.origin
-        window.location.href = url + `${import.meta.env.VITE_KNOWAGE_CONTEXT}/servlet/AdapterHTTP?PAGE=LoginPage&NEW_SESSION=TRUE`
+        window.location.href = `${url}${import.meta.env.VITE_KNOWAGE_CONTEXT}/servlet/AdapterHTTP?PAGE=LoginPage&NEW_SESSION=TRUE`
     }
 }
