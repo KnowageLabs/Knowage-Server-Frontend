6438c04f9048f394110fb3766d28b9a1a8507ff1
Davide Vernassa <davide.vernassa@eng.it>
2025-01-17 17:57:35 +0100

    Removed Debugger

--- src/components/UI/KnOverlaySpinnerPanel.vue
+++ src/components/UI/KnOverlaySpinnerPanel.vue
@@ -8,15 +8,12 @@
     const store = mainStore()
     store.$subscribe(
         (mutation,state) => {
-            debugger;
             if(mutation.events.key === 'loading'){
                 if (state.loading && state.loading > 0) $q.loading.show()
                 else $q.loading.hide()
             }
-            
         }
     )
 
-   
 </script>
 
