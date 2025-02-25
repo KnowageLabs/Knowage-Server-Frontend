65f65e8dda42e4ed3288b915a2fb769440bbf451
Bojan Sovtic <bojan.sovtic@eng.it>
2025-02-25 14:40:35 +0100

    Removed logs

--- src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/ChartWidget/highcharts/series/HighchartsSeriesAliasesSettings.vue
+++ src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/ChartWidget/highcharts/series/HighchartsSeriesAliasesSettings.vue
@@ -1,9 +1,6 @@
 <template>
     <div v-if="widgetModel" class="p-grid p-jc-center p-ai-center p-p-4">
         <div v-for="(aliasSettings, index) in chartAliasesSettings" :key="index" class="dynamic-form-item p-grid p-col-12 p-ai-center">
-            <div class="p-col-12">
-                {{ aliasSettings }}
-            </div>
             <div class="p-col-12 p-md-6 p-d-flex p-flex-column kn-flex">
                 <q-select v-model="aliasSettings.column" :options="columnOptions" :label="$t('qbe.entities.types.measure')" option-label="columnName" />
             </div>
@@ -51,12 +48,10 @@ const loadAliasesSettings = () => {
 }
 
 const filterAvailableOptionsAfterWidgetColumnsChanged = () => {
-    console.log('------ CAAAAAAAAAAAAAAAAAAAAAALED!')
     const validColumns = columnOptions.value.map((col) => ({ id: col.id, name: col.columnName }))
 
     const filteredAliases = widgetModel.value ? widgetModel.value.settings.series.aliases.filter((alias: { column: IWidgetColumn | null; alias: string }) => !alias.column || validColumns.some((validCol) => alias.column?.id === validCol.id || alias.column?.columnName === validCol.name)) : []
 
-    console.log('--------- FILTERED ALIASES: ', filteredAliases)
     if (filteredAliases.length === 0) filteredAliases.push({ column: null, alias: '' })
     chartAliasesSettings.value = filteredAliases
     if (widgetModel.value) {
