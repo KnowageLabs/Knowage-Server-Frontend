490c9e61f1b72a9dc63a6566265de48700c174ad
Bojan Sovtic <bojan.sovtic@eng.it>
2025-05-06 15:23:41 +0200

    Calculated field start

--- src/modules/documentExecution/dashboard/widget/WidgetEditor/MapWidget/MapWidgetLayerDetail.vue
+++ src/modules/documentExecution/dashboard/widget/WidgetEditor/MapWidget/MapWidgetLayerDetail.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="layer" class="p-d-flex p-flex-column kn-flex">
         <MapWidgetLayerInfo :selected-layer="layer" :layers="layers"></MapWidgetLayerInfo>
-        <MapWidgetMetadata class="p-mt-2" :selected-layer="layer"></MapWidgetMetadata>
+        <MapWidgetMetadata class="p-mt-2" :selected-layer="layer" :prop-widget="propWidget" :variables="variables"></MapWidgetMetadata>
     </div>
     <div v-else>
         <KnHint class="kn-hint-sm" :title="'dashboard.widgetEditor.map.layerHintTitle'" :hint="'dashboard.widgetEditor.map.layerHintText'" data-test="hint"></KnHint>
@@ -11,6 +11,7 @@
 <script lang="ts">
 import { PropType, defineComponent } from 'vue'
 import { ILayer, IMapWidgetLayer } from '../../../interfaces/mapWidget/DashboardMapWidget'
+import { IVariable, IWidget } from '@/modules/documentExecution/dashboard/Dashboard'
 import MapWidgetLayerInfo from './layerInfo/MapWidgetLayerInfo.vue'
 import MapWidgetMetadata from './metadata/MapWidgetMetadata.vue'
 import KnHint from '@/components/UI/KnHint.vue'
@@ -18,7 +19,7 @@ import KnHint from '@/components/UI/KnHint.vue'
 export default defineComponent({
     name: 'map-widget-layer-detail',
     components: { KnHint, MapWidgetLayerInfo, MapWidgetMetadata },
-    props: { selectedLayer: { type: Object as PropType<IMapWidgetLayer | null>, required: true }, layers: { type: Array as PropType<ILayer[]>, required: true } },
+    props: { selectedLayer: { type: Object as PropType<IMapWidgetLayer | null>, required: true }, layers: { type: Array as PropType<ILayer[]>, required: true }, propWidget: { type: Object as PropType<IWidget>, required: true }, variables: { type: Array as PropType<IVariable[]>, required: true } },
     data() {
         return {
             layer: null as IMapWidgetLayer | null
