e2f4397d7aa5c6666737c08f625ecb509841cee8
Davide Vernassa <davide.vernassa@eng.it>
2025-05-28 11:29:58 +0200

    Removed news error from CE version

--- src/App.vue
+++ src/App.vue
@@ -251,21 +251,26 @@ export default defineComponent({
 
                 if (!this.configurations['KNOWAGE.WEBSOCKET.DISABLE'] || this.configurations['KNOWAGE.WEBSOCKET.DISABLE'] === 'false') this.newsDownloadHandler()
             })
-            await this.$http
-                .get(import.meta.env.VITE_KNOWAGE_CONTEXT + '/restful-services/2.0/news')
-                .then(async (newsResponse) => {
-                    await this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + '/restful-services/2.0/newsRead').then((newsReadResponse) => {
-                        const json = { news: { count: { total: 0, unread: 0 } } }
-                        json.news.count.total = newsResponse.data.length
-                        json.news.count.unread = newsResponse.data.length - newsReadResponse.data.length
-                        this.setNews(json.news)
+            if (this.isEnterprise) {
+                await this.$http
+                    .get(import.meta.env.VITE_KNOWAGE_CONTEXT + '/restful-services/2.0/news')
+                    .then(async (newsResponse) => {
+                        await this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + '/restful-services/2.0/newsRead').then((newsReadResponse) => {
+                            const json = { news: { count: { total: 0, unread: 0 } } }
+                            json.news.count.total = newsResponse.data.length
+                            json.news.count.unread = newsResponse.data.length - newsReadResponse.data.length
+                            this.setNews(json.news)
+                        })
                     })
-                })
-                .catch((error) => {})
-                .finally(() => {
-                    this.loadInternationalization()
-                    this.setLoading(false)
-                })
+                    .catch((error) => {})
+                    .finally(() => {
+                        this.loadInternationalization()
+                        this.setLoading(false)
+                    })
+            } else {
+                this.loadInternationalization()
+                this.setLoading(false)
+            }
         },
         async loadInternationalization() {
             let currentLocale = localStorage.getItem('locale') ? localStorage.getItem('locale') : this.locale
