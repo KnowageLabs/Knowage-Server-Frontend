86db26d4e750e01909dff8949d9294a8b3e251f8
dbulatovicx32 <darko.bulatovic@eng.it>
2025-06-10 13:21:02 +0200

    KNOWAGE-9076
    - fixed dates in registry, problem was with aggrid type mismatch between date and string
    - tooltip date format will now match table one

--- src/modules/documentExecution/registry/tables/registryCellRenderers/RegistryTooltipRenderer.vue
+++ src/modules/documentExecution/registry/tables/registryCellRenderers/RegistryTooltipRenderer.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="custom-tooltip-container">
-        <span id="custom-tooltip-suffix">{{ params.value }}</span>
+        <span id="custom-tooltip-suffix">{{ getTooltip() }}</span>
     </div>
 </template>
 
 <script lang="ts">
+import { formatDateWithLocale, localeDate, luxonFormatDate, primeVueDate } from '@/helpers/commons/localeHelper'
+import { setInputDataType } from '@/helpers/commons/tableHelpers'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
@@ -16,6 +18,45 @@ export default defineComponent({
     },
     data() {
         return {}
+    },
+    computed: {
+        getCurrentLocaleDefaultDateFormat() {
+            return (column) => (column.isEditable ? column.format || primeVueDate() : localeDate())
+        }
+    },
+    methods: {
+        getTooltip() {
+            switch (this.getCellType(this.params.colDef)) {
+                case 'temporal':
+                    return this.getDateTooltip()
+                default:
+                    return this.params.value
+            }
+        },
+        getDateTooltip() {
+            if (!this.params.value) return ''
+
+            try {
+                if (this.params.colDef.columnInfo?.type === 'timestamp') {
+                    return formatDateWithLocale(this.params.value, { dateStyle: 'short', timeStyle: 'medium' }, true)
+                } else {
+                    return this.getFormattedDate(this.params.value, 'yyyy-MM-dd', this.getCurrentLocaleDefaultDateFormat(this.params.colDef))
+                }
+            } catch (error) {
+                return this.params.value || ''
+            }
+        },
+
+        getFormattedDate(date: any, format: any, incomingFormat?: string) {
+            return luxonFormatDate(date, format, incomingFormat)
+        },
+        getCellType(colDef) {
+            if (colDef.editorType == 'TEXT' && colDef.columnInfo.type === 'boolean') return 'checkbox'
+            if (colDef.editorType !== 'COMBO' && colDef.columnInfo?.type !== 'date' && colDef.columnInfo?.type !== 'timestamp' && setInputDataType(colDef.columnInfo?.type) === 'text') return 'text'
+            if (colDef.editorType !== 'COMBO' && colDef.columnInfo?.type !== 'date' && colDef.columnInfo?.type !== 'timestamp' && setInputDataType(colDef.columnInfo?.type) === 'number') return 'number'
+            if (colDef.editorType === 'COMBO') return 'dropdown'
+            if (colDef.columnInfo?.type === 'date' || colDef.columnInfo?.type === 'timestamp') return 'temporal'
+        }
     }
 })
 </script>
