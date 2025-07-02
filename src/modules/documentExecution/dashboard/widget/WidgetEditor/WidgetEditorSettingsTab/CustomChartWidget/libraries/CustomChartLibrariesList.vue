d08355284e4b85960d5255408030467363a4adf4
Davide Vernassa <davide.vernassa@eng.it>
2025-07-02 11:07:22 +0200

    Added hint to custom chart libraries

--- src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/CustomChartWidget/libraries/CustomChartLibrariesList.vue
+++ src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/CustomChartWidget/libraries/CustomChartLibrariesList.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
         <div class="p-formgrid p-grid p-p-3">
-            <q-select class="p-lg-12" filled v-model="model" :options="options" use-input input-debounce="0" new-value-mode="add" :label="$t('dashboard.widgetEditor.libraries.select')" @new-value="addLibrary" @update:model-value="addLibrary" />
+            <q-select class="p-lg-12" filled v-model="model" :options="options" use-input input-debounce="0" bottom-slots new-value-mode="add" :label="$t('dashboard.widgetEditor.libraries.select')" @new-value="addLibrary" @update:model-value="addLibrary" >
+                <template v-slot:hint>
+                    {{ $t('dashboard.widgetEditor.libraries.hint') }}
+                </template>
+            </q-select>
 
             <q-list v-if="selectedLibraries.length !== 0" class="p-lg-12" bordered separator>
                 <q-item v-for="(library, index) in selectedLibraries" :key="index" clickable v-ripple>
