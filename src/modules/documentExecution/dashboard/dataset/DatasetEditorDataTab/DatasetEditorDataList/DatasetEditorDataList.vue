3856f45d47a29dd7bf11429336a462bdadf71cd2
dbulatovicx32 <darkobulatovicx32@gmail.com>
2025-07-09 15:44:30 +0200

    KNOWAGE-9229
    - disable delete if dataset is being used by a widget in dashboard model

--- src/modules/documentExecution/dashboard/dataset/DatasetEditorDataTab/DatasetEditorDataList/DatasetEditorDataList.vue
+++ src/modules/documentExecution/dashboard/dataset/DatasetEditorDataTab/DatasetEditorDataList/DatasetEditorDataList.vue
@@ -2,23 +2,13 @@
     <div class="dashboard-editor-list-card-container p-m-3">
         <div class="dashboard-editor-list-card">
             <Button label="Add Dataset" icon="pi pi-plus-circle" class="p-button-outlined p-mt-2 p-mx-2" data-test="add-dataset-button" @click="toggleDataDialog"></Button>
-            <Listbox
-                class="kn-list kn-list-no-border-right dashboard-editor-list"
-                :options="selectedDatasets"
-                :filter="true"
-                :filter-placeholder="$t('common.search')"
-                option-label="label"
-                filter-match-mode="contains"
-                :filter-fields="['label']"
-                :empty-filter-message="$t('common.info.noDataFound')"
-                @change="selectDataset"
-            >
+            <Listbox class="kn-list kn-list-no-border-right dashboard-editor-list" :options="selectedDatasets" :filter="true" :filter-placeholder="$t('common.search')" option-label="label" filter-match-mode="contains" :filter-fields="['label']" :empty-filter-message="$t('common.info.noDataFound')" @change="selectDataset">
                 <template #empty>{{ $t('common.info.noDataFound') }}</template>
                 <template #option="slotProps">
                     <div v-tooltip.left="slotProps.option.label" class="kn-list-item" :style="dataListDescriptor.style.list.listItem" data-test="dataset-list-item">
                         <i class="p-mx-2" :style="dataListDescriptor.style.list.listIcon" :class="dataListDescriptor.listboxSettings.avatar.values[slotProps.option.type].icon"></i>
                         <span class="kn-list-item-text">{{ slotProps.option.label }}</span>
-                        <Button icon="far fa-trash-alt" class="p-button-text p-button-rounded p-button-plain p-ml-auto" data-test="delete-dataset-list-item" @click.stop="deleteDatasetFromModel(slotProps.option)" />
+                        <Button :disabled="isDatasetUsed(slotProps.option.id.dsId)" icon="far fa-trash-alt" class="p-button-text p-button-rounded p-button-plain p-ml-auto" data-test="delete-dataset-list-item" @click.stop="deleteDatasetFromModel(slotProps.option)" />
                     </div>
                 </template>
             </Listbox>
@@ -30,7 +20,7 @@
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
-import { IDataset } from '../../../Dashboard'
+import { IDashboard, IDataset } from '../../../Dashboard'
 import Listbox from 'primevue/listbox'
 import DataDialog from '../DatasetEditorDataDialog/DatasetEditorDataDialog.vue'
 import dashStore from '../../../Dashboard.store'
@@ -76,6 +66,20 @@ export default defineComponent({
         },
         selectDataset(event) {
             this.$emit('datasetSelected', event.value.id.dsId)
+        },
+        isDatasetUsed(datasetId) {
+            const dashboardList = this.dashboardStore.getDashboardsList() as IDashboard
+            if (!dashboardList || !datasetId) return false
+
+            for (const dashboard of Object.values(dashboardList)) {
+                if (dashboard.widgets && Array.isArray(dashboard.widgets)) {
+                    for (const widget of dashboard.widgets) {
+                        if (widget.dataset === datasetId) return true
+                    }
+                }
+            }
+
+            return false
         }
     }
 })
