b80b70a30dec891f629581773099abd8de4d6e72
Davide Vernassa <davide.vernassa@eng.it>
2025-07-18 17:48:35 +0200

    Fixed multiple same name routes

--- src/modules/documentExecution/main/DocumentExecutionMain.routes.js
+++ src/modules/documentExecution/main/DocumentExecutionMain.routes.js
@@ -1,18 +1,18 @@
 const routes = [
     {
         path: '/dashboard/new-dashboard',
-        name: 'document-execution',
-        component: () => import('@/modules/documentExecution/main/DocumentExecution.vue'),
-        props: true
-    },
-    {
-        path: '/:mode(registry|document-composite|report|office-doc|olap|map|report|kpi|dossier|etl)/:id',
-        name: 'document-execution',
+        name: 'dashboard-new',
         component: () => import('@/modules/documentExecution/main/DocumentExecution.vue'),
         props: true
     },
     {
         path: '/dashboard/:id',
+        name: 'dashboard-execution',
+        component: () => import('@/modules/documentExecution/main/DocumentExecution.vue'),
+        props: true
+    },
+    {
+        path: '/:mode(registry|document-composite|report|office-doc|olap|map|kpi|dossier|etl)/:id',
         name: 'document-execution',
         component: () => import('@/modules/documentExecution/main/DocumentExecution.vue'),
         props: true
