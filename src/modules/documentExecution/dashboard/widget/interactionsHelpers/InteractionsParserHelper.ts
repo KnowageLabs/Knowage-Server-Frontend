9af23c6df6f6966e471cb1bb457b3ffd34c242fe
dbulatovicx32 <darkobulatovicx32@gmail.com>
2025-08-05 13:02:06 +0200

    KNOWAGE-9185
    - check if original string exists, sometimes it can be an empty one

--- src/modules/documentExecution/dashboard/widget/interactionsHelpers/InteractionsParserHelper.ts
+++ src/modules/documentExecution/dashboard/widget/interactionsHelpers/InteractionsParserHelper.ts
@@ -2,6 +2,8 @@ import { IDashboardDriver, IVariable } from '../../Dashboard'
 import { columnFieldRegex, parameterTextCompatibilityRegex, variableTextCompatibilityRegex } from '../../helpers/common/DashboardRegexHelper'
 
 export const replaceVariablesPlaceholdersByVariableName = (originalString: string, variables: IVariable[]) => {
+    if (!originalString) return originalString
+
     originalString = originalString.replace(variableTextCompatibilityRegex, (match: string, variableName: string) => {
         if (variables && variables.length > 0) {
             const dashboardVariable = variables.find((variable: IVariable) => variable.name === variableName)
@@ -13,6 +15,8 @@ export const replaceVariablesPlaceholdersByVariableName = (originalString: strin
 }
 
 export const replaceDriversPlaceholdersByDriverUrlName = (originalString: string, drivers: IDashboardDriver[]) => {
+    if (!originalString) return originalString
+
     originalString = originalString.replace(parameterTextCompatibilityRegex, (match: string, parameterName: string) => {
         if (drivers && drivers.length > 0) {
             const dashboardVariable = drivers.find((driver: IDashboardDriver) => driver.urlName === parameterName)
@@ -24,6 +28,8 @@ export const replaceDriversPlaceholdersByDriverUrlName = (originalString: string
 }
 
 export const replaceDriversPlaceholdersByDriverName = (originalString: string, drivers: IDashboardDriver[]) => {
+    if (!originalString) return originalString
+
     originalString = originalString.replace(parameterTextCompatibilityRegex, (match: string, parameterName: string) => {
         if (drivers && drivers.length > 0) {
             const dashboardVariable = drivers.find((driver: IDashboardDriver) => driver.name === parameterName)
@@ -35,6 +41,8 @@ export const replaceDriversPlaceholdersByDriverName = (originalString: string, d
 }
 
 export const replaceFieldPlaceholdersByColumnName = (originalString: string, formattedRow: any) => {
+    if (!originalString) return originalString
+
     originalString = originalString.replace(columnFieldRegex, (match: string, fieldName: string) => {
         return formattedRow[fieldName] ? formattedRow[fieldName].value : ''
     })
