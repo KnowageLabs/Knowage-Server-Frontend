56cc4bd54d22c0d4027f82d699f1a76b9de0cc2d
dbulatovicx32 <darkobulatovicx32@gmail.com>
2025-08-07 16:34:32 +0200

    KNOWAGE-9236
    - remove logs

--- src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorDataTab/WidgetEditorDataList/WidgetEditorDataList.vue
+++ src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorDataTab/WidgetEditorDataList/WidgetEditorDataList.vue
@@ -1,6 +1,5 @@
 <template>
     <div v-if="widgetModel" class="dashboard-editor-list-card-container p-m-3">
-        {{ isSolrDataset }}
         <span class="p-float-label p-mx-2 p-mt-4 p-mb-1">
             <Dropdown id="dataset" v-model="selectedDataset" class="kn-material-input kn-width-full" :options="datasetOptions" option-label="label" @change="onDatasetSelected"></Dropdown>
             <label for="dataset" class="kn-material-input-label"> {{ $t('dashboard.widgetEditor.selectDataset') }} </label>
