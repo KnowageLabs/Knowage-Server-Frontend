d3aa5f808cf513df9d9d92d971e2d8259a807248
dbulatovicx32 <darkobulatovicx32@gmail.com>
2025-10-01 14:55:46 +0200

    KNOWAGE-9429
    - hide on streamgraph because it serves no purpose

--- src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/ChartWidget/highcharts/configuration/HighchartsAxisLinesSettings.vue
+++ src/modules/documentExecution/dashboard/widget/WidgetEditor/WidgetEditorSettingsTab/ChartWidget/highcharts/configuration/HighchartsAxisLinesSettings.vue
@@ -1,14 +1,14 @@
 <template>
     <div v-if="axisLinesSettings" class="p-grid p-jc-center p-ai-center p-p-4">
-        <div class="p-col-12 p-md-4">
+        <div v-if="chartType !== 'streamgraph'" class="col-grow">
             <WidgetEditorColorPicker :initial-value="axisLinesSettings.color" :label="$t('common.color')" @change="onSelectionColorChanged($event, 'color')"></WidgetEditorColorPicker>
         </div>
 
-        <div class="p-col-12 p-md-4">
+        <div class="col-grow p-mx-2">
             <WidgetEditorColorPicker :initial-value="axisLinesSettings.crosshairColor" :label="$t('dashboard.widgetEditor.highcharts.axisLines.crosshairColor')" @change="onSelectionColorChanged($event, 'crosshairColor')"></WidgetEditorColorPicker>
         </div>
 
-        <div class="p-col-12 p-md-4 p-d-flex p-flex-column p-mb-4">
+        <div class="col-grow p-d-flex p-flex-column p-mb-4">
             <label class="kn-material-input-label p-mr-2">{{ $t('dashboard.widgetEditor.highcharts.axisLines.crosshairWidth') }}</label>
             <InputNumber v-model="axisLinesSettings.crosshairWidth" class="kn-material-input p-inputtext-sm" />
         </div>
@@ -30,6 +30,11 @@ export default defineComponent({
         return {
             axisLinesSettings: null as any,
             getTranslatedLabel
+        }
+    },
+    computed: {
+        chartType() {
+            return this.widgetModel?.settings.chartModel?.model?.chart.type
         }
     },
     created() {
