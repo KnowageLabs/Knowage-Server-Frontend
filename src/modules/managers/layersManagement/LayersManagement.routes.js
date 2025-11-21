6662041983c733eccfd43024055ff141bab2a175
Davide Vernassa <davide.vernassa@eng.it>
2025-11-21 17:00:16 +0100

    Removed GeoLayersManagement

--- src/modules/managers/layersManagement/LayersManagement.routes.js
+++ src/modules/managers/layersManagement/LayersManagement.routes.js
@@ -2,7 +2,6 @@ const routes = [
     {
         path: '/layers-management',
         name: 'layers-management',
-        meta: { functionality: 'GeoLayersManagement' },
         component: () => import('@/modules/managers/layersManagement/LayersManagement.vue')
     }
 ]
