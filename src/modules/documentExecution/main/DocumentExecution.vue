aeeb7500f3bd0518130080fbf2d98ddd093ea634
Davide Vernassa <davide.vernassa@eng.it>
2026-01-15 16:26:48 +0100

    [KNOWAGE-9640] Added id watcher

--- src/modules/documentExecution/main/DocumentExecution.vue
+++ src/modules/documentExecution/main/DocumentExecution.vue
@@ -359,6 +359,12 @@ export default defineComponent({
             this.filtersData = {} as any
             await this.loadDocument()
             this.userRole ? await this.loadPage(true) : (this.parameterSidebarVisible = true)
+        },
+        id(newId, oldId) {
+            if (newId !== oldId && newId && this.configurations && Object.keys(this.configurations).length > 0) {
+                this.breadcrumbs = []
+                this.initialize()
+            }
         }
     },
     async activated() {
@@ -368,6 +374,7 @@ export default defineComponent({
         this.parameterSidebarVisible = false
         window.removeEventListener('message', this.iframeEventsListener)
     },
+
     async mounted() {
         this.$q.loading.show()
         this.initializePolling = setInterval(() => {
@@ -831,7 +838,6 @@ export default defineComponent({
         async loadDocument() {
             await this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + `/restful-services/2.0/documents/${this.document?.label}`).then((response: AxiosResponse<any>) => (this.document = response.data))
             const index = this.breadcrumbs.findIndex((el: any) => el.label === this.document.name)
-            debugger
             index !== -1
                 ? (this.breadcrumbs[index].document = this.document)
                 : this.breadcrumbs.push({
