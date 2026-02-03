381334e16d508da6c0e49bba4838b54395cc6da3
Emanuele Cerilli <emanuele.cerilli@eng.it>
2026-02-03 14:52:21 +0100

    refactor behaviouralModelLineage

--- src/modules/managers/behaviouralModelLineage/BehaviouralModelLineage.vue
+++ src/modules/managers/behaviouralModelLineage/BehaviouralModelLineage.vue
@@ -10,10 +10,10 @@
         </q-toolbar>
 
         <div id="table-container" class="bml-table-container">
-            <BMLTable :table-data="allDocuments" :header-title="$t('managers.datasetManagement.documents')" :loading="loading" data-type="documents" @rowSelected="onRowSelect" @rowUnselected="onRowUnselect" />
-            <BMLTable :table-data="allDatasets" :header-title="$t('common.datasets')" :loading="loading" data-type="datasets" @rowSelected="onRowSelect" @rowUnselected="onRowUnselect" />
-            <BMLTable :table-data="allDrivers" :header-title="$t('managers.bml.drivers')" :loading="loading" data-type="analyticalDrivers" @rowSelected="onRowSelect" @rowUnselected="onRowUnselect" />
-            <BMLTable :table-data="allLovs" :header-title="$t('managers.bml.lovsTitle')" :loading="loading" data-type="lovs" @rowSelected="onRowSelect" @rowUnselected="onRowUnselect" />
+            <BMLTable :table-data="allDocuments" :header-title="$t('common.documents')" :loading="tableLoading.documents" :selection-locked="selectionLocked" data-type="documents" @rowSelected="onRowSelect" @rowUnselected="onRowUnselect" />
+            <BMLTable :table-data="allDatasets" :header-title="$t('common.dataset')" :loading="tableLoading.datasets" :selection-locked="selectionLocked" data-type="datasets" @rowSelected="onRowSelect" @rowUnselected="onRowUnselect" />
+            <BMLTable :table-data="allDrivers" :header-title="$t('common.driver')" :loading="tableLoading.drivers" :selection-locked="selectionLocked" data-type="analyticalDrivers" @rowSelected="onRowSelect" @rowUnselected="onRowUnselect" />
+            <BMLTable :table-data="allLovs" :header-title="$t('managers.bml.lovsTitle')" :loading="tableLoading.lovs" :selection-locked="selectionLocked" data-type="lovs" @rowSelected="onRowSelect" @rowUnselected="onRowUnselect" />
         </div>
     </div>
 </template>
@@ -40,6 +40,12 @@ export default defineComponent({
         return {
             descriptor,
             loading: false,
+            tableLoading: {
+                documents: false,
+                datasets: false,
+                drivers: false,
+                lovs: false
+            } as Record<string, boolean>,
             filterFields: ['name', 'label'],
             filters: { global: [filterDefault] } as Object,
             allLovs: [] as iLov[],
@@ -49,33 +55,65 @@ export default defineComponent({
             allDatasets: [] as iDataset[]
         }
     },
+    computed: {
+        selectionLocked(): boolean {
+            return Object.values(this.tableLoading).some((v) => v)
+        }
+    },
     created() {
         this.loadAllData()
     },
     methods: {
         async loadAllData() {
             this.loading = true
-            await Promise.all([this.loadAllLovs(), this.loadAllDrivers(), this.loadAllDocuments(), this.loadAllDatasets()]).then(() => (this.loading = false))
+            this.tableLoading = { documents: true, datasets: true, drivers: true, lovs: true }
+            await Promise.all([this.loadAllLovs(), this.loadAllDrivers(), this.loadAllDocuments(), this.loadAllDatasets()]).finally(() => {
+                this.loading = false
+            })
         },
         async loadAllLovs() {
-            await this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + '/restful-services/2.0/lovs/get/all/').then((response: AxiosResponse<any>) => {
-                this.allLovs = response.data
-            })
+            this.tableLoading.lovs = true
+            await this.$http
+                .get(import.meta.env.VITE_KNOWAGE_CONTEXT + '/restful-services/2.0/lovs/get/all/')
+                .then((response: AxiosResponse<any>) => {
+                    this.allLovs = response.data
+                })
+                .finally(() => {
+                    this.tableLoading.lovs = false
+                })
         },
         async loadAllDrivers() {
-            await this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + '/restful-services/2.0/analyticalDrivers').then((response: AxiosResponse<any>) => {
-                this.allDrivers = response.data
-            })
+            this.tableLoading.drivers = true
+            await this.$http
+                .get(import.meta.env.VITE_KNOWAGE_CONTEXT + '/restful-services/2.0/analyticalDrivers')
+                .then((response: AxiosResponse<any>) => {
+                    this.allDrivers = response.data
+                })
+                .finally(() => {
+                    this.tableLoading.drivers = false
+                })
         },
         async loadAllDocuments() {
-            await this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + '/restful-services/2.0/documents').then((response: AxiosResponse<any>) => {
-                this.allDocuments = response.data
-            })
+            this.tableLoading.documents = true
+            await this.$http
+                .get(import.meta.env.VITE_KNOWAGE_CONTEXT + '/restful-services/2.0/documents')
+                .then((response: AxiosResponse<any>) => {
+                    this.allDocuments = response.data
+                })
+                .finally(() => {
+                    this.tableLoading.documents = false
+                })
         },
         async loadAllDatasets() {
-            await this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + '/restful-services/2.0/datasets').then((response: AxiosResponse<any>) => {
-                this.allDatasets = response.data
-            })
+            this.tableLoading.datasets = true
+            await this.$http
+                .get(import.meta.env.VITE_KNOWAGE_CONTEXT + '/restful-services/2.0/datasets')
+                .then((response: AxiosResponse<any>) => {
+                    this.allDatasets = response.data
+                })
+                .finally(() => {
+                    this.tableLoading.datasets = false
+                })
         },
         onRowSelect(event, dataType) {
             switch (dataType) {
@@ -98,42 +136,58 @@ export default defineComponent({
         },
         async filterByLovs(lov) {
             this.loading = true
+            this.tableLoading = { documents: true, datasets: true, drivers: true, lovs: false }
             await this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + `/restful-services/2.0/lovs/${lov.id}/analyticalDrivers/`).then((response: AxiosResponse<any>) => (this.allDrivers = response.data))
             await this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + `/restful-services/2.0/lovs/${lov.id}/documents/`).then((response: AxiosResponse<any>) => (this.allDocuments = response.data))
+            this.tableLoading.drivers = false
+            this.tableLoading.documents = false
             // Also fetch datasets related to the returned documents
+            this.tableLoading.datasets = true
             const documentDatasetsPromises = this.allDocuments.map(async (doc: iDocument) => {
                 return this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + `/restful-services/1.0/documents/${doc.id}/dataset/`).then((response: AxiosResponse<any>) => response.data)
             })
             const documentDatasetsArrays = await Promise.all(documentDatasetsPromises)
             const allDatasets = documentDatasetsArrays.flat()
             this.allDatasets = allDatasets
+            this.tableLoading.datasets = false
             this.loading = false
         },
         async filterByDrivers(driver) {
             this.loading = true
+            this.tableLoading = { documents: true, datasets: true, drivers: false, lovs: true }
             await this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + `/restful-services/2.0/analyticalDrivers/${driver.id}/lovs/`).then((response: AxiosResponse<any>) => (this.allLovs = response.data))
             await this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + `/restful-services/2.0/analyticalDrivers/${driver.id}/documents/`).then((response: AxiosResponse<any>) => (this.allDocuments = response.data))
+            this.tableLoading.lovs = false
+            this.tableLoading.documents = false
 
+            this.tableLoading.datasets = true
             const documentDatasetsPromises = this.allDocuments.map(async (doc: iDocument) => {
                 return this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + `/restful-services/1.0/documents/${doc.id}/dataset/`).then((response: AxiosResponse<any>) => response.data)
             })
             const documentDatasetsArrays = await Promise.all(documentDatasetsPromises)
             const allDatasets = documentDatasetsArrays.flat()
             this.allDatasets = allDatasets
+            this.tableLoading.datasets = false
             this.loading = false
         },
         async filterByDocuments(doc) {
             this.loading = true
+            this.tableLoading = { documents: false, datasets: true, drivers: true, lovs: true }
             await this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + `/restful-services/1.0/documents/${doc.label}/lovs/`).then((response: AxiosResponse<any>) => (this.allLovs = response.data))
             await this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + `/restful-services/1.0/documents/${doc.label}/analyticalDrivers/`).then((response: AxiosResponse<any>) => (this.allDrivers = response.data))
+            this.tableLoading.lovs = false
+            this.tableLoading.drivers = false
 
             const documentDatasets = await this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + `/restful-services/1.0/documents/${doc.id}/dataset/`).then((response: AxiosResponse<any>) => response.data)
             this.allDatasets = documentDatasets
+            this.tableLoading.datasets = false
             this.loading = false
         },
         async filterByDatasets(dataset) {
             this.loading = true
+            this.tableLoading = { documents: true, datasets: false, drivers: true, lovs: true }
             await this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + `/restful-services/2.0/datasets/${dataset.id.dsId}/documents/`).then((response: AxiosResponse<any>) => (this.allDocuments = response.data))
+            this.tableLoading.documents = false
             // Also fetch LOVs and Drivers related to the returned documents
             const documentLovsPromises = this.allDocuments.map(async (doc: iDocument) => {
                 return this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + `/restful-services/1.0/documents/${doc.label}/lovs/`).then((response: AxiosResponse<any>) => response.data)
@@ -141,6 +195,7 @@ export default defineComponent({
             const documentLovsArrays = await Promise.all(documentLovsPromises)
             const allLovs = documentLovsArrays.flat()
             this.allLovs = allLovs
+            this.tableLoading.lovs = false
 
             const documentDriversPromises = this.allDocuments.map(async (doc: iDocument) => {
                 return this.$http.get(import.meta.env.VITE_KNOWAGE_CONTEXT + `/restful-services/1.0/documents/${doc.label}/analyticalDrivers/`).then((response: AxiosResponse<any>) => response.data)
@@ -148,6 +203,7 @@ export default defineComponent({
             const documentDriversArrays = await Promise.all(documentDriversPromises)
             const allDrivers = documentDriversArrays.flat()
             this.allDrivers = allDrivers
+            this.tableLoading.drivers = false
             this.loading = false
         }
     }
--- src/modules/managers/behaviouralModelLineage/BehaviouralModelLineageTable.vue
+++ src/modules/managers/behaviouralModelLineage/BehaviouralModelLineageTable.vue
@@ -17,25 +17,30 @@
 
             <q-separator />
 
-            <q-inner-loading :showing="!!loading">
-                <q-spinner size="36px" color="primary" />
-            </q-inner-loading>
+            <div class="bml-table-wrap">
+                <q-inner-loading :showing="!!loading">
+                    <q-spinner size="36px" color="primary" />
+                </q-inner-loading>
 
-            <q-table class="kn-table" :rows="tableData" :columns="columns" :row-key="rowKey" dense flat :filter="filter" :pagination="pagination" :rows-per-page-options="[10, 25, 50, 100]" separator="horizontal" selection="single" v-model:selected="selected" @selection="onSelection">
-                <template #body-cell-label="props">
-                    <q-td :props="props">
-                        <q-chip dense square color="grey-2" text-color="grey-9" class="q-ma-none" :title="props.row.label">
-                            <span class="ellipsis">{{ props.row.label }}</span>
-                        </q-chip>
-                    </q-td>
-                </template>
+                <!-- When ANY table is still loading, we must prevent clicks on rows entirely -->
+                <div v-if="selectionLocked" class="bml-click-blocker" aria-hidden="true" />
 
-                <template #no-data>
-                    <div class="full-width row flex-center q-pa-md text-grey-7">
-                        {{ $t('common.info.noDataFound') }}
-                    </div>
-                </template>
-            </q-table>
+                <q-table :key="renderKey" class="kn-table" :rows="tableData" :columns="columns" :row-key="rowKey" dense flat :filter="filter" :pagination="pagination" :rows-per-page-options="[10, 25, 50, 100]" separator="horizontal" row-class="bml-row" :row-class-fn="rowClass" @row-click="onRowClick">
+                    <template #body-cell-label="props">
+                        <q-td :props="props" :class="{ 'bml-td--selected': isRowSelected(props.row) }">
+                            <q-chip dense square class="q-ma-none bml-chip" :class="{ 'bml-chip--selected': isRowSelected(props.row) }" :title="props.row.label">
+                                <span class="ellipsis">{{ props.row.label }}</span>
+                            </q-chip>
+                        </q-td>
+                    </template>
+
+                    <template #no-data>
+                        <div class="full-width row flex-center q-pa-md text-grey-7">
+                            {{ $t('common.info.noDataFound') }}
+                        </div>
+                    </template>
+                </q-table>
+            </div>
         </q-card>
     </div>
 </template>
@@ -63,16 +68,18 @@ export default defineComponent({
         tableData: { type: Array as any, required: true },
         headerTitle: { type: String, required: false, default: '' },
         dataType: { type: String, required: false, default: '' },
-        loading: { type: Boolean, required: false, default: false }
+        loading: { type: Boolean, required: false, default: false },
+        selectionLocked: { type: Boolean, required: false, default: false }
     },
     emits: ['rowSelected', 'rowUnselected'],
     data() {
         return {
             filter: '',
-            selected: [] as any[],
+            selectedKey: null as string | null,
+            renderKey: 0,
             pagination: { rowsPerPage: 25 } as any,
             // Datasets expose the unique key as row.id.dsId; other entities usually have a flat `id`.
-            rowKey: (row: any) => row?.id?.dsId ?? row?.id ?? row?.dsId ?? row?.label ?? row?.name,
+            rowKey: (row: any) => String(row?.id?.dsId ?? row?.id ?? row?.dsId ?? row?.label ?? row?.name ?? ''),
             columns: [
                 {
                     name: 'label',
@@ -102,17 +109,37 @@ export default defineComponent({
     },
     watch: {
         tableData() {
-            this.selected = []
+            // Force QTable to re-evaluate row classes after rows refresh.
+            this.renderKey++
+            if (this.selectedKey === null) return
+            const hasSelected = Array.isArray(this.tableData) && this.tableData.some((r: any) => (this.rowKey as any)(r) === this.selectedKey)
+            if (!hasSelected) this.selectedKey = null
         }
     },
     methods: {
-        onSelection(details: any) {
-            if (details?.added) {
-                const row = (details.rows && details.rows[0]) || (this.selected && this.selected[0])
-                if (row) this.$emit('rowSelected', { data: row }, this.dataType)
+        isRowSelected(row: any) {
+            const key = (this.rowKey as any)(row)
+            return this.selectedKey !== null && key === this.selectedKey
+        },
+        rowClass(row: any) {
+            const key = (this.rowKey as any)(row)
+            const selected = this.selectedKey !== null && key === this.selectedKey ? 'bml-row--selected' : ''
+            const locked = this.selectionLocked ? 'bml-row--locked' : ''
+            return [selected, locked].filter(Boolean).join(' ')
+        },
+        onRowClick(_evt: any, row: any) {
+            if (this.selectionLocked) return
+            const key = (this.rowKey as any)(row) as string
+
+            // toggle selection
+            if (this.selectedKey !== null && key === this.selectedKey) {
+                this.selectedKey = null
+                this.$emit('rowUnselected', {}, this.dataType)
                 return
             }
-            this.$emit('rowUnselected', {}, this.dataType)
+
+            this.selectedKey = key
+            this.$emit('rowSelected', { data: row }, this.dataType)
         }
     }
 })
@@ -139,8 +166,49 @@ export default defineComponent({
     background: rgba(0, 0, 0, 0.03);
 }
 
+.bml-qtable .q-table tbody tr.bml-row.bml-row--selected,
+.bml-qtable .q-table tbody tr.bml-row.bml-row--selected:hover,
+.bml-qtable .q-table tbody tr.bml-row.bml-row--selected:active {
+    background: rgba(25, 118, 210, 0.16);
+    box-shadow: inset 3px 0 0 rgba(25, 118, 210, 0.9);
+}
+
+.bml-qtable .q-table tbody tr.bml-row.bml-row--selected td {
+    font-weight: 600;
+}
+
+.bml-qtable .q-table .bml-chip {
+    background: rgba(0, 0, 0, 0.06);
+    color: rgba(0, 0, 0, 0.9);
+}
+
+.bml-qtable .q-table .bml-chip.bml-chip--selected {
+    background: rgba(25, 118, 210, 0.22);
+    color: rgba(0, 0, 0, 0.92);
+}
+
+.bml-qtable .q-table td.bml-td--selected {
+    box-shadow: inset 3px 0 0 rgba(25, 118, 210, 0.9);
+}
+
+.bml-qtable .q-table tbody tr.bml-row--locked {
+    cursor: not-allowed;
+}
+
+.bml-qtable .bml-table-wrap {
+    position: relative;
+    flex: 1;
+}
+
+.bml-qtable .bml-click-blocker {
+    position: absolute;
+    inset: 0;
+    z-index: 5;
+    cursor: wait;
+    background: transparent;
+}
+
 .bml-qtable .ellipsis {
     max-width: 100%;
 }
 </style>
-},
