cc97034a50dbd40be7913c86b25b4a3387302e8e
dbulatovicx32 <darkobulatovicx32@gmail.com>
2026-04-24 15:00:06 +0200

    KNOWAGE-9804
    - update search on widget dialog to comply with quasar
    - fix missing labels and hints
    - show alias instead of column name

--- src/modules/documentExecution/dashboard/widget/WidgetSearchDialog/WidgetSearchDialog.vue
+++ src/modules/documentExecution/dashboard/widget/WidgetSearchDialog/WidgetSearchDialog.vue
@@ -1,39 +1,61 @@
 <template>
-    <Dialog class="kn-dialog--toolbar--primary" :visible="visible" :header="$t('dashboard.widgetEditor.searchInTableWidget')" :style="descriptor.dialogStyle" :closable="false" modal>
-        <div v-if="widget" class="p-formgrid p-grid">
-            <Message class="p-text-center p-col-12" severity="info" :closable="false">{{ $t('dashboard.widgetEditor.searchInTableWidgetHint') }}</Message>
+    <q-dialog :model-value="visible" persistent>
+        <q-card :style="descriptor.dialogStyle">
+            <q-toolbar class="kn-toolbar kn-toolbar--primary">
+                <q-toolbar-title>{{ $t('dashboard.widgetEditor.searchInTableWidget') }}</q-toolbar-title>
+                <q-btn flat round dense icon="help_outline">
+                    <q-tooltip anchor="top middle" :delay="100">{{ $t('dashboard.widgetEditor.searchInTableWidgetHint') }}</q-tooltip>
+                </q-btn>
+            </q-toolbar>
 
-            <div class="p-field p-float-label p-col-12">
-                <InputText v-model="searchText" class="kn-material-input kn-width-full" data-test="search-input" />
-                <label class="kn-material-input-label">{{ $t('common.search') }}</label>
-            </div>
+            <q-card-section v-if="widget" class="q-px-md q-pb-md kn-width-full">
+                <div class="row q-col-gutter-sm">
+                    <div class="col-6">
+                        <q-select v-model="searchColumns" :options="columnOptions" :label="$t('common.columns')" option-label="alias" option-value="columnName" emit-value map-options multiple dense outlined class="kn-width-full">
+                            <template v-slot:before-options>
+                                <q-item clickable @click="toggleAll">
+                                    <q-item-section side>
+                                        <q-checkbox :model-value="allSelected" :indeterminate="someSelected && !allSelected" dense @update:model-value="toggleAll" />
+                                    </q-item-section>
+                                </q-item>
+                                <q-separator />
+                            </template>
+                            <template v-slot:option="scope">
+                                <q-item v-bind="scope.itemProps">
+                                    <q-item-section side>
+                                        <q-checkbox :model-value="scope.selected" dense @update:model-value="scope.toggleOption(scope.opt)" />
+                                    </q-item-section>
+                                    <q-item-section>
+                                        <q-item-label>{{ scope.opt.alias }}</q-item-label>
+                                    </q-item-section>
+                                </q-item>
+                            </template>
+                        </q-select>
+                    </div>
+                    <div class="col-6">
+                        <q-input v-model="searchText" :label="$t('common.search')" dense outlined class="kn-width-full" data-test="search-input" />
+                    </div>
+                </div>
+            </q-card-section>
 
-            <div class="p-field p-float-label p-col-12">
-                <MultiSelect v-model="searchColumns" :options="columnOptions" class="kn-width-full" option-label="columnName" option-value="columnName"> </MultiSelect>
-                <label for="attributes" class="kn-material-input-label"> {{ $t('common.columns') }} </label>
-            </div>
-        </div>
+            <q-separator />
 
-        <template #footer>
-            <div class="p-d-flex p-flex-row p-jc-end">
-                <Button class="kn-button kn-button--primary" data-test="close-button" @click="closeDialog"> {{ $t('common.close') }}</Button>
-                <Button class="kn-button kn-button--primary" @click="search"> {{ $t('common.search') }}</Button>
-            </div>
-        </template>
-    </Dialog>
+            <q-card-actions align="right">
+                <q-btn unelevated data-test="close-button" @click="closeDialog">{{ $t('common.close') }}</q-btn>
+                <q-btn unelevated color="primary" @click="search">{{ $t('common.search') }}</q-btn>
+            </q-card-actions>
+        </q-card>
+    </q-dialog>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import { IWidget, IWidgetColumn } from '../../Dashboard'
-import Dialog from 'primevue/dialog'
 import descriptor from './WidgetSearchDialogDescriptor.json'
-import Message from 'primevue/message'
-import MultiSelect from 'primevue/multiselect'
 
 export default defineComponent({
     name: 'widget-search-dialog',
-    components: { Dialog, Message, MultiSelect },
+    components: {},
     props: { visible: { required: true, type: Boolean }, widget: { type: Object as PropType<IWidget>, required: true }, propSearch: { type: Object as PropType<{ searchText: string; searchColumns: string[] }>, required: true } },
     emits: ['close', 'search'],
     data() {
@@ -50,6 +72,12 @@ export default defineComponent({
         },
         searchButtonDisabled() {
             return this.searchColumns.length === 0
+        },
+        allSelected() {
+            return this.columnOptions.length > 0 && this.searchColumns.length === this.columnOptions.length
+        },
+        someSelected() {
+            return this.searchColumns.length > 0
         }
     },
     watch: {
@@ -68,6 +96,10 @@ export default defineComponent({
             if (!this.propSearch) return
             this.searchText = this.propSearch.searchText
             this.searchColumns = [...this.propSearch.searchColumns]
+        },
+        toggleAll() {
+            if (this.allSelected) this.searchColumns = []
+            else this.searchColumns = this.columnOptions.map((col: IWidgetColumn) => col.columnName)
         },
         closeDialog() {
             this.$emit('close')
