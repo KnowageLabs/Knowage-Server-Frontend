8e20049f0e3a1d960dbf5ad5b40bca8eef5aec07
eng-dedalus <emanuele.cerilli@eng.it>
2026-09-16 14:51:33 +0200

    add carto endpoint

--- src/modules/documentExecution/dashboard/widget/MapWidget/MapBaseLayerHelper.ts
+++ src/modules/documentExecution/dashboard/widget/MapWidget/MapBaseLayerHelper.ts
@@ -9,6 +9,7 @@ export interface IMapWidgetBaseLayerDefinition {
         attribution: string
         subdomains?: string
     }
+    requiresAuthentication?: boolean
 }
 
 export const DEFAULT_MAP_BASE_LAYER: IMapWidgetBaseLayerType = 'cartoLight'
@@ -26,22 +27,22 @@ const mapBaseLayerDefinitions: Record<IMapWidgetBaseLayerType, IMapWidgetBaseLay
     cartoLight: {
         value: 'cartoLight',
         labelKey: 'dashboard.widgetEditor.map.baseLayer.cartoLight',
-        url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+        url: `${import.meta.env.VITE_KNOWAGE_CONTEXT}/restful-services/2.0/map-tiles/carto/light/{z}/{x}/{y}.png`,
         options: {
             maxZoom: 20,
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
-            subdomains: 'abcd'
-        }
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
+        },
+        requiresAuthentication: true
     },
     cartoDark: {
         value: 'cartoDark',
         labelKey: 'dashboard.widgetEditor.map.baseLayer.cartoDark',
-        url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+        url: `${import.meta.env.VITE_KNOWAGE_CONTEXT}/restful-services/2.0/map-tiles/carto/dark/{z}/{x}/{y}.png`,
         options: {
             maxZoom: 20,
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
-            subdomains: 'abcd'
-        }
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
+        },
+        requiresAuthentication: true
     },
     esriSatellite: {
         value: 'esriSatellite',
